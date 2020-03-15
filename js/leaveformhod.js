3e911d71167a31ff258730c39612cfca788d94e8
Sai Srihitha Reddy.T <42039283+SrihithaReddy13@users.noreply.github.com>
2020-03-15 23:40:17 +0530

    Add files via upload

--- js/leaveformhod.js
+++ js/leaveformhod.js
@@ -1,28 +1,15 @@
-$(document).ready(function(){
-  $('#leavesdiv').show();
-  $('#salarydiv').hide();
-  $('#leaves').click(function(){
-    $('#leavesdiv').show();
-    $('#salarydiv').hide();
-  });
-  $('#salary').click(function(){
-    $('#salarydiv').show();
-    $('#leavesdiv').hide();
-  });
-});
-
 
 function openLeaves(){
-  x=document.getElementById("leavesdiv");
+  var x=document.getElementById("leavesdiv");
   x.style.display="block";
-  w=document.getElementById("salarydiv");
+  var w=document.getElementById("salarydiv");
   w.style.display="none";
 }
 
 
 function openSalary(){
-  x=document.getElementById("leavesdiv");
+  var x=document.getElementById("leavesdiv");
   x.style.display="none";
-  w=document.getElementById("salarydiv");
+  var w=document.getElementById("salarydiv");
   w.style.display="block";
 }
